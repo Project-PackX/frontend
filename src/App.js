2140e14bd725216167a2e1c97a526534c61c0a5c
Artúr Friedrich <artur.friedrich.harka@gmail.com>
2023-09-26 11:20:41 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import axios from "axios";
+
 function App() {
+
+  const getPackages = async () => {
+    axios.get('http://localhost:4444/api/packages')
+    .then((response) => {
+      console.log(response.data);
+    })
+    .catch((error) => {
+      console.log(error);
+    })
+  };
+
   return (
     <div>
       <h1>React App</h1>
+      <button onClick={getPackages}>Get Packages</button>
     </div>
   );
 }

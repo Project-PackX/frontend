9467257df2e7d5ea6406c605856429df1027b279
Artúr Friedrich <artur.friedrich.harka@gmail.com>
2023-09-29 14:47:19 +0200

    routes added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
 import axios from "axios";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+import { Navbar } from './components/Navbar/Navbar';
+
 
 function App() {
 
@@ -14,8 +19,16 @@ function App() {
 
   return (
     <div>
-      <h1>React App</h1>
-      <button onClick={getPackages}>Get Packages</button>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<h1>Home</h1>} />
+          <Route path="/track" element={<h1>Track</h1>} />
+          <Route path="/aboutus" element={<h1>About us</h1>} />
+          <Route path="/contactus" element={<h1>Contact us</h1>} />
+          <Route path="/login" element={<h1>Login</h1>} />
+        </Routes>
+      </Router>
     </div>
   );
 }

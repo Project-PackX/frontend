8f179f01b474b0a7b65607173098dd7dd714b60c
Artúr Friedrich <artur.friedrich.harka@gmail.com>
2023-10-02 11:03:38 +0200

    get package by packageID

--- src/views/Track/Track.js
+++ src/views/Track/Track.js
@@ -1,52 +1,50 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import PackageDataService from "../../services/package.js";
-import axios from 'axios';
 
 import './track.css';
 
 export const Track = () => {
-
-    const getPackages = async () => {
-        axios.get('http://localhost:4444/api/packages')
-        .then((response) => {
-          console.log(response.data);
-        })
-        .catch((error) => {
-          console.log(error);
-        })
-      };
-
-    /* const [packageID, setPackageID] = useState('');
-
-    useEffect(() => {
-        getPackageById();
-    }, []);
+    const [packageID, setPackageID] = useState('');
+    const [packageData, setPackageData] = useState(null);
 
     const searchPackage = (e) => {
-        e.preventDefault();
         setPackageID(e.target.value);
     }
 
-    const getPackageById = async () => {
+    const getPackageById = () => {
         PackageDataService.get(packageID)
-        .then((response) => {
-            console.log(response.data);
-        })
-        .catch((error) => {
-            console.log(error);
-        })
-    } */
+            .then((response) => {
+                setPackageData(response.data);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }
 
     return (
         <div className="track-container mx-5">
             <h1 className="track-title">Track & trace</h1>
             <div className="track-input-container">
-                {/* <form className='track-form'>
-                    <input type='text' value={packageID} onChange={searchPackage} className="track-input my-3" placeholder="Enter your tracking number" />
-                    <button onSubmit={getPackageById} className="button track-button mx-3">Track</button>
-                </form> */}
-                <button onClick={getPackages}>Get Packages</button>
+                <form className='track-form' onSubmit={(e) => {
+                    e.preventDefault();
+                    getPackageById();
+                }}>
+                    <input
+                        type='text'
+                        value={packageID}
+                        onChange={(e) => searchPackage(e)}
+                        className="track-input my-3"
+                        placeholder="Enter your tracking number"
+                    />
+                    <button type="submit" className="button track-button mx-3">Track</button>
+                </form>
             </div>
+            {packageData && (
+                <div className="package-details">
+                    <h2>Package Details:</h2>
+                    <pre>{JSON.stringify(packageData, null, 2)}</pre>
+                </div>
+            )}
         </div>
     )
 }

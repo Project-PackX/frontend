af1f2faccfd3f9d50e0b7653b2f815cf6d979efb
Artúr Friedrich <artur.friedrich.harka@gmail.com>
2023-10-02 14:43:15 +0200

    style update and added package information

--- src/views/Track/Track.js
+++ src/views/Track/Track.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import PackageDataService from "../../services/package.js";
+import { PackageStatus } from '../PackageStatus/PackageStatus.js';
 
 import './track.css';
 
@@ -19,32 +20,60 @@ export const Track = () => {
             .catch((error) => {
                 console.log(error);
             });
+            /* const jsonData = {
+                "Data": {
+                  "ID": 1,
+                  "CreatedAt": "2023-10-02T13:45:53.031287+02:00",
+                  "UpdatedAt": "2023-10-02T13:45:53.031287+02:00",
+                  "DeletedAt": null,
+                  "TrackID": "",
+                  "UserID": 2,
+                  "SenderLockerId": "",
+                  "DestinationLockerId": "",
+                  "Size": "Medium",
+                  "Price": 37990,
+                  "Code": "",
+                  "DeliveryDate": "2023-10-02T13:45:53.031287+02:00",
+                  "Note": "Utánvét",
+                  "CourierID": 1
+                },
+                "Status": "In Warehouse"
+              };
+          
+              // Set the JSON data as the packageData state
+              setPackageData(jsonData); */
     }
 
     return (
         <div className="track-container mx-5">
-            <h1 className="track-title">Track & trace</h1>
-            <div className="track-input-container">
-                <form className='track-form' onSubmit={(e) => {
-                    e.preventDefault();
-                    getPackageById();
-                }}>
-                    <input
-                        type='text'
-                        value={packageID}
-                        onChange={(e) => searchPackage(e)}
-                        className="track-input my-3"
-                        placeholder="Enter your tracking number"
-                    />
-                    <button type="submit" className="button track-button mx-3">Track</button>
-                </form>
-            </div>
-            {packageData && (
-                <div className="package-details">
-                    <h2>Package Details:</h2>
-                    <pre>{JSON.stringify(packageData, null, 2)}</pre>
-                </div>
-            )}
+            {
+                !packageData ? (
+                    <>
+                        <h1 className="track-title">Track Shipment</h1>
+                        <h1 className="track-subtitle">Enter a product number to start tracking</h1>
+                        <div className="track-input-container">
+                            <form className='track-form' onSubmit={(e) => {
+                                e.preventDefault();
+                                getPackageById();
+                            }}>
+                                <div className="input-with-icon">
+                                    <img src={require('../../assets/icons/search.png')} alt="track" className="search-icon" />
+                                    <input
+                                        type='text'
+                                        value={packageID}
+                                        onChange={(e) => searchPackage(e)}
+                                        className="track-input my-3"
+                                        placeholder="Enter your tracking number"
+                                    />
+                                </div>
+                                <button type="submit" className="button track-button mx-3">Track</button>
+                            </form>
+                        </div>
+                    </>
+                ) : (
+                    <PackageStatus packageData={packageData} />
+                )
+            }
         </div>
     )
 }

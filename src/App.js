4ac2eb80e105caff06a6de95b6511e7a0b9e665a
Artúr Friedrich <artur.friedrich.harka@gmail.com>
2023-10-03 21:55:02 +0200

    added register view

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { Navbar } from './components/Navbar/Navbar';
 
 import { Track } from './views/Track/Track';
 import { Login } from './views/Login/Login';
-
+import { Register } from './views/Register/Register';
 
 function App() {
 
@@ -19,6 +19,7 @@ function App() {
           <Route path="/aboutus" element={<h1>About us</h1>} />
           <Route path="/contactus" element={<h1>Contact us</h1>} />
           <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
         </Routes>
       </Router>
     </div>
---
+++ src/views/Register/Register.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export const Register = () => {
+    return (
+        <div className="container">
+            <div className="row justify-content-center">
+                <div className="col-md-6">
+                    <div className="card">
+                        <div className="card-header">Register</div>
+                        <div className="card-body">
+                            <form>
+                                <div className="mb-3">
+                                    <label htmlFor="name" className="form-label">Name</label>
+                                    <input type="text" className="form-control" id="name" placeholder="Enter your name" />
+                                </div>
+                                <div className="mb-3">
+                                    <label htmlFor="email" className="form-label">Email</label>
+                                    <input type="email" className="form-control" id="email" placeholder="Enter your email" />
+                                </div>
+                                <div className="mb-3">
+                                    <label htmlFor="password" className="form-label">Password</label>
+                                    <input type="password" className="form-control" id="password" placeholder="Enter your password" />
+                                </div>
+                                <div className="mb-3">
+                                    <label htmlFor="passwordConfirm" className="form-label">Confirm Password</label>
+                                    <input type="password" className="form-control" id="passwordConfirm" placeholder="Confirm your password" />
+                                </div>
+                                <div className="mb-3">
+                                    <label htmlFor="town" className="form-label">Town</label>
+                                    <input type="text" className="form-control" id="town" placeholder="Enter your town" />
+                                </div>
+                                <div className="mb-3 form-check">
+                                    <input type="checkbox" className="form-check-input" id="acceptTerms" />
+                                    <label className="form-check-label" htmlFor="acceptTerms">I accept the terms and conditions</label>
+                                </div>
+                                <button type="submit" className="btn btn-primary">Register</button>
+                            </form>
+                        </div>
+                        <div className="card-footer">
+                            Already have an account? <Link to="/login">Login here</Link>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};

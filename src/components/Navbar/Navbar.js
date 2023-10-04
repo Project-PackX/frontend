3d76384836b1979c1c0b1d159911ef4666c4e2c2
MrHumanRebel <szekelydani5g@gmail.com>
2023-10-04 17:43:44 +0200

    feat(logo): add full white logo

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -7,7 +7,7 @@ export const Navbar = () => {
   return (
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
       <Link to="/" class="navbar-brand mx-5">
-        <img className="navbar-logo" src={require("../../assets/images/packx_logo_white.png")} alt="logo" />
+        <img className="navbar-logo" src={require("../../assets/images/packx_logo_full_white.png")} alt="logo" />
       </Link>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>

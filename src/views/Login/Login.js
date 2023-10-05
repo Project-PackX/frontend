0f12a1c9bc3d98f8da93af4671905444b754209b
Artúr Friedrich <artur.friedrich.harka@gmail.com>
2023-10-05 09:21:09 +0200

    login page design #1

--- src/views/Login/Login.js
+++ src/views/Login/Login.js
@@ -5,29 +5,36 @@ import './login.css';
 
 export const Login = () => {
     return (
-        <div className="container">
-            <div className="row justify-content-center">
-                <div className="col-md-6">
-                    <div className="card">
-                        <div className="card-header">Login</div>
-                        <div className="card-body">
-                            <form>
-                                <div className="mb-3">
-                                    <label htmlFor="email" className="form-label">Email</label>
-                                    <input type="email" className="form-control" id="email" placeholder="Enter your email" />
-                                </div>
-                                <div className="mb-3">
-                                    <label htmlFor="password" className="form-label">Password</label>
-                                    <input type="password" className="form-control" id="password" placeholder="Enter your password" />
-                                </div>
-                                <button type="submit" className="btn btn-primary">Login</button>
-                            </form>
-                        </div>
-                        <div className="card-footer">
-                            Don't have an account? <Link to="/register">Register here</Link>
-                        </div>
+        <div className="login container row col-12">
+            <div className="form-container col-md-6 mt-5">
+                <h1 className="title">Welcome back!</h1>
+                <p className="subtitle">Please enter your details</p>
+                <form>
+                    <div className="mb-3">
+                        <label htmlFor="username" className="form-label">Username</label>
+                        <input
+                            type="text"
+                            className="form-input"
+                            id="username"
+                            required
+                        />
                     </div>
-                </div>
+                    <div className="mb-3">
+                        <label htmlFor="password" className="form-label">Password</label>
+                        <input
+                            type="password"
+                            className="form-input"
+                            id="password"
+                            required
+                        />
+                    </div>
+
+                    <button type="submit" className="login-btn">Log In</button>
+                </form>
+                <p className="register-text">Don't have an account?<Link className="register-link" to="/register"> Sign Up</Link></p>
+            </div>
+            <div className="col-md-6">
+                <img src={require("../../assets/images/undraw_delivery_truck_vt6p.svg").default} alt="login" />
             </div>
         </div>
     );

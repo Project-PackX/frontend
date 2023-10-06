5036ed84e0fcccf32f55cb1d35a182df1094cc35
Artúr Friedrich <artur.friedrich.harka@gmail.com>
2023-10-06 10:37:21 +0200

    improved track design

--- src/views/Track/Track.js
+++ src/views/Track/Track.js
@@ -1,7 +1,5 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom'
-import PackageDataService from "../../services/package.js";
-import { PackageStatus } from '../PackageStatus/PackageStatus.js';
 
 import './track.css';
 
@@ -20,26 +18,31 @@ export const Track = () => {
     }
 
     return (
-        <div className="track-container mx-5">
-            <h1 className="track-title">Track Shipment</h1>
-            <h1 className="track-subtitle">Enter a product number to start tracking</h1>
-            <div className="track-input-container">
-                <form className='track-form' onSubmit={(e) => {
-                    e.preventDefault();
-                    getPackageById();
-                }}>
-                    <div className="input-with-icon">
-                        <img src={require('../../assets/icons/search.png')} alt="track" className="search-icon" />
-                        <input
-                            type='text'
-                            value={packageID}
-                            onChange={(e) => searchPackage(e)}
-                            className="track-input my-3"
-                            placeholder="Enter your tracking number"
-                        />
-                    </div>
-                    <button type="submit" className="button track-button mx-3">Track</button>
-                </form>
+        <div className="tracking container row col-12">
+            <div className="form-container col-md-6">
+                <h1 className="track-title">Package Tracking</h1>
+                <h1 className="track-subtitle">You can track your package status by entering your tracking number, which we send you via email.</h1>
+                <div className="track-input-container">
+                    <form className='track-form' onSubmit={(e) => {
+                        e.preventDefault();
+                        getPackageById();
+                    }}>
+                        <div className="input-with-icon">
+                            <img src={require('../../assets/icons/search.png')} alt="track" className="search-icon" />
+                            <input
+                                type='text'
+                                value={packageID}
+                                onChange={(e) => searchPackage(e)}
+                                className="track-input my-3"
+                                placeholder="Enter your tracking number"
+                            />
+                        </div>
+                        <button type="submit" className="button track-button mx-3">Track</button>
+                    </form>
+                </div>
+            </div>
+            <div className="col-md-6">
+                <img className="track-image" src={require("../../assets/images/undraw_current_location_re_j130.svg").default} alt="login" />
             </div>
         </div>
     )

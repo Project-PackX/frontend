ac319788ac34e5d91f615e6aecf59271aebe4f23
MrHumanRebel <szekelydani5g@gmail.com>
2023-10-07 10:47:53 +0200

    feat(webpack): fix module not found

---
+++ src/views/Contact/Contact.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './contact.css';
+
+export const Contact = () => {
+    return (
+        <div className="container">
+            <div className="row justify-content-center">
+                <div className="col-md-6">
+                    <h1>Contact Us Page</h1>
+                </div>
+            </div>
+        </div>
+    );
+};

c0a7f3ea2c6e879aab5739bf633b88dc3d8d8033
Artúr Friedrich <artur.friedrich.harka@gmail.com>
2023-10-07 13:06:04 +0200

    commit

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,15 +1,26 @@
 import React from 'react';
-
 import './home.css';
 
 export const Home = () => {
     return (
-        <div className="container">
-            <div className="row justify-content-center">
-                <div className="col-md-6">
-                    <h1>Landing Page</h1>
+        <>
+            <div className="background-image home-container">
+                <div className="text">
+                    <h1 className="title">Welcome to PackX</h1>
+                    <p className="subtitle">The home of safe 'n fast deliveries.</p>
+                    <button type="submit" className="home-btn">Send Package</button>
                 </div>
             </div>
-        </div>
+            <div className="seperator-line"></div>
+            <div className="home-container">
+                <div className="text">
+                    <h2 className="title content-left">Deliveries made easy</h2>
+                    <p>No more worries about packages.</p>
+                </div>
+                <div className="image-right">
+                    <img src={require("../../assets/images/undraw_takeout_boxes_ap54.svg").default} alt="login" />
+                </div>
+            </div>
+        </>
     );
-};
+}

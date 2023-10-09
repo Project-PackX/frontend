763a0367f919252eeaec2cda5d0bebd17766d3b8
MrHumanRebel <szekelydani5g@gmail.com>
2023-10-09 10:39:12 +0200

    feat(contact): add base page

--- src/views/Contact/Contact.js
+++ src/views/Contact/Contact.js
@@ -4,12 +4,29 @@ import './contact.css';
 
 export const Contact = () => {
     return (
-        <div className="container">
-            <div className="row justify-content-center">
-                <div className="col-md-6">
-                    <h1>Contact Us Page</h1>
+        <>
+            <div className="data-container">
+                <div className="text">
+                    <h2 className="title content-left">Management warehouse</h2>
+                    <p className="data" >Hungary Győr-Moson-Sopron 9999 Kismacska Nagymacska street 69. </p>
+                    <p className="data" >+36 99 999 99 99 </p>
+                    <p className="data" >info@packx.com </p>
+                </div>
+                <div className="content-right">
+                <p className="data" >MAP HERE. </p>
                 </div>
             </div>
-        </div>
+            <div className="seperator-line"></div>
+            <div className="data-container">
+                <div className="content-left">
+                <p className="data" >MAP HERE. </p>
+                </div>
+                <div className="text">
+                    <h2 className="title content-right">Package point locations</h2>
+                    <p className="data" >LOCATIONS DROPDOWN HERE. </p>
+                    <button type="select" className="select-btn">Select</button>
+                </div>
+            </div>           
+        </>
     );
-};
+}

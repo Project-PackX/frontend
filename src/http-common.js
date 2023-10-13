79d9001fe47640d9f1ef231d238e63590d9ca5b6
Artúr Friedrich <artur.friedrich.harka@gmail.com>
2023-10-13 12:57:43 +0200

    updated api uri

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "http://packx-sandbox.overlab.hu:8000/api/packages",
+    baseURL: "http://packx-sandbox.overlab.hu:8000/api",
     headers: {
         "Content-type": "application/json"
     }

7bdc748391dc00d9e309cfcefdfa761be1e035bd
Artúr Friedrich <artur.friedrich.harka@gmail.com>
2023-10-13 12:58:54 +0200

    updated packages routes

--- src/services/package.js
+++ src/services/package.js
@@ -2,12 +2,13 @@ import http from "../http-common";
 
 class PackageDataService {
     getAll() {
-        return http.get("/");
+        return http.get("/packages");
     }
 
     get(id) {
-        return http.get(`/${id}`);
+        return http.get(`/packages/${id}`);
     }
 }
 
+// eslint-disable-next-line import/no-anonymous-default-export
 export default new PackageDataService();

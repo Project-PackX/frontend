ab26a749c2a8c6d75740af2f7bb48d5c7ca8a2f3
Artúr Friedrich <artur.friedrich.harka@gmail.com>
2023-10-16 18:33:18 +0200

    (home): little style change

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,77 +1,73 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './home.css';
 
 export const Home = () => {
     return (
-        <>
-            <div className="background-image home-container">
-                <div className="text">
-                    <h1 className="title">Welcome to PackX</h1>
-                    <p className="subtitle">The home of safe 'n fast deliveries.</p>
-                    <button type="submit" className="home-btn">Send Package</button>
-                </div>
-            </div>
-            <div className="seperator-line"></div>
-            <div className="data-container">
-                <div className="text">
-                    <h2 className="title content-left">Deliveries made easy</h2>
-                    <p className="data-subtitle-left">No more worries about packages.</p>
-                    <p className="data" >Our team members are effortlessly working 
-                    to make every one of our customers delighted. PackX’s networking
-                     service provides exceptional care to both the customer and the package,
-                      while maintaining quality.</p>
-                </div>
-                <div className="image-right">
-                    <img src={require("../../assets/images/undraw_takeout_boxes_ap54.svg").default} alt="login" />
-                </div>
-            </div>
-            <div className="seperator-line"></div>
-            <div className="data-container">
-                <div className="image-left">
-                    <img src={require("../../assets/images/undraw_takeout_boxes_ap54.svg").default} alt="login" />
-                </div>
-                <div className="text">
-                    <h2 className="title content-right">Always on time</h2>
-                    <p className="data-subtitle-right">In a hurry? We got you covered.</p>
-                    <p className="data" >We all now that feeling when a crucial item is
-                     on its way to us, and we feel that the package is never arriving. 
-                     PackX offers a 3 day guarantee on deliveries in certain countries 1*. 
-                    You can relax and our service will take care of the rest.</p>
-                </div>
-            </div>
-            <div className="seperator-line"></div>
-            <div className="trusted-awards-container">
-                <h2 className="title">Trusted by</h2>
-                <div className="logo-container">
-                    <div className="logo-column">
-                        <img src={require("../../assets/logos/otp.png")} alt="Logo 1" />
+        <main>
+            <div className="container">
+                <div className="hero row col-12">
+                    <div className="hero-content col-md-6">
+                        <h1>Make your life easier <br/> with <span>PackX!</span></h1>
+                        <p>Send and receive packages was never quicker and easier.</p>
+                        <Link to="/login" className="button button-accent">Get Started</Link>
                     </div>
-                    <div className="logo-column">
-                        <img src={require("../../assets/logos/aqua.png")} alt="Logo 2" />
-                    </div>
-                    <div className="logo-column">
-                        <img src={require("../../assets/logos/alza.png")} alt="Logo 3" />
-                    </div>
-                    <div className="logo-column">
-                        <img src={require("../../assets/logos/emag.png")} alt="Logo 4" />
+                    <div className="hero-image col-md-6">
+                        <img src={require('../../assets/images/undraw_delivery_truck_vt6p.svg').default} alt="hero" />
                     </div>
                 </div>
             </div>
-            <div className="seperator-line"></div>
-            <div className="trusted-awards-container">
-                <h2 className="title">Awards and certificates</h2>
-                <div className="logo-container">
-                    <div className="logo-column">
-                        <img src={require("../../assets/logos/pay.png")} alt="Logo 1" />
+            <div className="features row col-12">
+                <div className="container">
+                    <div className="feature col-md-4">
+                        <h3>Really fast</h3>
+                        <p>With the PackX Rapid Delivery your package arrives in 3 business days to your selected destination.</p>
                     </div>
-                    <div className="logo-column">
-                        <img src={require("../../assets/logos/startup.png")} alt="Logo 2" />
+                    <div className="feature col-md-4">
+                        <h3>Eco-friendly</h3>
+                        <p>Our couriers are using 100% electric delivery trucks, we save tons of CO<sub>2</sub> every day.</p>
                     </div>
-                    <div className="logo-column">
-                        <img src={require("../../assets/logos/iso.png")} alt="Logo 3" />
+                    <div className="feature col-md-4">
+                        <h3>24/7 Accessibility</h3>
+                        <p>Our package delivery service is available around the clock, ensuring you can send or receive your packages at any time that suits you.</p>
                     </div>
                 </div>
             </div>
-        </>
+            <div className="container">
+                <div className="sending row col-12">
+                    <div className="title col-md-6">
+                        <h2>Send your package now</h2>
+                    </div>
+                    <div className="image col-md-6">
+                        <p>Send your package easily from a locker to another one.</p>
+                    </div>
+                </div>
+                <div className="sending-boxes row col-12">
+                    <div className="sending-box col-md-4">
+                        <h2>Create an account</h2>
+                        <img src={require('../../assets/images/undraw_female_avatar_efig.svg').default} alt="box" />
+                        <p>You can only dispatch a package if you have a PackX Account</p>
+                    </div>
+                    <div className="sending-box col-md-4">
+                        <h2>Give the package details</h2>
+                        <img src={require('../../assets/images/undraw_server_status_re_n8ln.svg').default} alt="box" />
+                        <p>You can select the closest locker to you and the destination locker too. Select the size of the package and the delivery method.</p>
+                    </div>
+                    <div className="sending-box col-md-4">
+                        <h2>Track your package</h2>
+                        <img src={require('../../assets/images/undraw_location_tracking_re_n3ok.svg').default} alt="box" />
+                        <p>You can follow along your package and get a notification when it arrives at the destination.</p>
+                    </div>
+                </div>
+            </div>
+            <div className="container">
+                <div className="receiving row col-12">
+                    <div className="title col-md-6">
+                    </div>
+                    <div className="image col-md-6">
+                    </div>
+                </div>
+            </div>
+        </main>
     );
 }

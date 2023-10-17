255669f838ac24f7a94c7c0a86328046c5ad945a
Artúr Friedrich <artur.friedrich.harka@gmail.com>
2023-10-17 17:04:28 +0200

    (dispatch): new package endpoint

--- src/services/package.js
+++ src/services/package.js
@@ -4,9 +4,15 @@ class PackageDataService {
     getAll() {
         return http.get("/packages");
     }
-
     get(id) {
         return http.get(`/packages/get/${id}`);
+    }
+    new(data, token) {
+        return http.post("/packages/new", data, {
+            headers: {
+                Authorization: `${token}`
+            }
+        });
     }
 }
 

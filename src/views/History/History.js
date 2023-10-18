4941df165f775b0ba355449b8fe1014b883b91e6
Artúr Friedrich <artur.friedrich.harka@gmail.com>
2023-10-18 15:59:45 +0200

    (history): better look and track button

--- src/views/History/History.js
+++ src/views/History/History.js
@@ -1,33 +1,36 @@
 import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import UserDataService from '../../services/user';
 import decode from 'jwt-decode';
-
-import "./history.css"
+import './history.css'; // Import your custom CSS file
 
 export const History = () => {
-    const [history, setHistory] = useState([])
+    const [history, setHistory] = useState([]);
 
     const loadHistory = () => {
-        UserDataService.history(decode(localStorage.getItem("token")).user_id, localStorage.getItem("token"))
+        UserDataService.history(
+            decode(localStorage.getItem('token')).user_id,
+            localStorage.getItem('token')
+        )
             .then((response) => {
                 setHistory(response.data.message);
             })
             .catch((error) => {
-                console.error("Error while loading history", error);
+                console.error('Error while loading history', error);
             });
-    }
+    };
 
     useEffect(() => {
         loadHistory();
     }, []);
 
     return (
-        <div>
+        <div className="history-container">
             <h1>History</h1>
             <div className="row">
                 {history.map((item, index) => (
                     <div className="col-md-4" key={index}>
-                        <div className="card mb-4">
+                        <div className="history-card">
                             <div className="history-item">
                                 <h5 className="card-title">Package ID: {item.ID}</h5>
                                 <p className="card-text">Created At: {item.CreatedAt}</p>
@@ -38,10 +41,13 @@ export const History = () => {
                                 <p className="card-text">Delivery Date: {item.DeliveryDate}</p>
                                 <p className="card-text">Note: {item.Note}</p>
                             </div>
+                            <Link to={`/track/${item.TrackID}`} className="btn login-btn">
+                                Track
+                            </Link>
                         </div>
                     </div>
                 ))}
             </div>
         </div>
     );
-}
+};

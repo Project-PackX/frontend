b0ced9da6e5a74fc3a3723149ae8e50ca48a2ab2
MrHumanRebel <szekelydani5g@gmail.com>
2023-10-24 23:51:53 +0200

    feat(cursor): bugfixes

--- src/components/CustomCursor/CustomCursor.js
+++ src/components/CustomCursor/CustomCursor.js
@@ -1,18 +1,22 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import AnimatedCursor from "react-animated-cursor";
 import "./customcursor.css";
 
 export default function CustomCursor() {
+  const cursorStyle = {
+    innerSize: 13,
+    outerSize: 13,
+    color: '142, 154, 175',
+    outerAlpha: 0.2,
+    innerScale: 0.7,
+    outerScale: 5,
+    trailingSpeed: 5,
+  };
+
   return (
     <div className="App">
       <AnimatedCursor
-        innerSize={13}
-        outerSize={13}
-        color='142, 154, 175'
-        outerAlpha={0.2}
-        innerScale={0.7}
-        outerScale={5}
-        trailingSpeed={5}
+        {...cursorStyle}
         clickables={[
           'a',
           'input[type="text"]',
@@ -22,9 +26,18 @@ export default function CustomCursor() {
           'input[type="image"]',
           'label[for]',
           'select',
+          'select option',
           'textarea',
           'button',
           '.link',
+          '.dropdown-menu',
+          'dropdown-menu',
+          '.form-select',
+          'form-select',
+          '.form-input',
+          'form-input',
+          '.form-check-input',
+          'form-check-input',
         ]}
         hideNativeCursor={true}
         style={{ zIndex: 9999 }}

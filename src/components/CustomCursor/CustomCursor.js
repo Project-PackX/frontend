93cc3bf2a68e39bba532529d57e4b3bbfbde64c1
MrHumanRebel <szekelydani5g@gmail.com>
2023-10-24 11:25:08 +0200

    feat(cursor): optimise code

--- src/components/CustomCursor/CustomCursor.js
+++ src/components/CustomCursor/CustomCursor.js
@@ -1,49 +1,12 @@
 import React, { useState, useEffect } from "react";
 import AnimatedCursor from "react-animated-cursor";
+import "./customcursor.css";
+
+const defaultCursorColor = '127, 90, 246';
+const alternativeCursorColor = '0, 159, 219';
 
 export default function CustomCursor() {
-  const defaultCursorColor = '127, 90, 246';
-  const alternativeCursorColor = '0, 159, 219';
-
   const [cursorColor, setCursorColor] = useState(defaultCursorColor);
-  const [lastColorChangeTime, setLastColorChangeTime] = useState(0);
-
-  useEffect(() => {
-    const updateCursorColor = (event) => {
-      if (!event.clientX || !event.clientY) {
-        return;
-      }
-
-      const elementUnderCursor = document.elementFromPoint(event.clientX, event.clientY);
-
-      if (!elementUnderCursor) {
-        return;
-      }
-
-      const elementStyles = window.getComputedStyle(elementUnderCursor);
-      const backgroundColor = elementStyles.backgroundColor;
-
-      if (isSimilarColor(backgroundColor, cursorColor, 0.55)) {
-        if (cursorColor !== defaultCursorColor) {
-          setCursorColor(defaultCursorColor);
-          setLastColorChangeTime(Date.now());
-        }
-      } else {
-        if (cursorColor !== alternativeCursorColor) {
-          setCursorColor(alternativeCursorColor);
-          setLastColorChangeTime(Date.now());
-        }
-      }
-
-      requestAnimationFrame(updateCursorColor);
-    };
-
-    document.addEventListener('mousemove', updateCursorColor);
-
-    return () => {
-      document.removeEventListener('mousemove', updateCursorColor);
-    };
-  }, [cursorColor, lastColorChangeTime]);
 
   const isSimilarColor = (color1, color2, threshold) => {
     const rgb1 = color1.match(/\d+/g).map(Number);
@@ -55,6 +18,34 @@ export default function CustomCursor() {
 
     return distance / maxDistance < threshold;
   };
+
+  useEffect(() => {
+    const updateCursorColor = (event) => {
+      if (!event.clientX || !event.clientY) return;
+
+      const elementUnderCursor = document.elementFromPoint(event.clientX, event.clientY);
+      if (!elementUnderCursor) return;
+
+      const elementStyles = window.getComputedStyle(elementUnderCursor);
+      const backgroundColor = elementStyles.backgroundColor;
+
+      if (isSimilarColor(backgroundColor, cursorColor, 0.60)) {
+        if (cursorColor !== defaultCursorColor) setCursorColor(defaultCursorColor);
+      } else {
+        if (cursorColor !== alternativeCursorColor) setCursorColor(alternativeCursorColor);
+      }
+    };
+
+    const onMouseMove = (event) => {
+      requestAnimationFrame(() => updateCursorColor(event));
+    };
+
+    document.addEventListener('mousemove', onMouseMove);
+
+    return () => {
+      document.removeEventListener('mousemove', onMouseMove);
+    };
+  }, [cursorColor]);
 
   return (
     <div className="App">
@@ -79,6 +70,7 @@ export default function CustomCursor() {
           'button',
           '.link',
         ]}
+        hideNativeCursor={true}
         style={{ zIndex: 9999 }}
       />
     </div>

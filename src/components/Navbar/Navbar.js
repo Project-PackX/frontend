dc8ea392213a301572086ea1c7f2c73fc814f009
Artúr Friedrich <artur.friedrich.harka@gmail.com>
2023-10-25 20:52:20 +0200

    (navbar): hamburger menu working

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -13,11 +13,18 @@ export const Navbar = () => {
       <Link to="/" class="navbar-brand mx-5">
         <img className="navbar-logo" src={require("../../assets/images/packx_logo_full_white.png")} alt="logo"/>
       </Link>
-      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-              aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
         <span className="navbar-toggler-icon"></span>
       </button>
-      <div className="collapse navbar-collapse justify-content-end align-items-end mx-5" id="navbarSupportedContent">
+      <div className="collapse navbar-collapse justify-content-end align-items-end mx-5" id="navbarNav">
         <ul className="navbar-nav mr-auto">
           {isLoggedIn ? (
             <li className="nav-item dropdown">

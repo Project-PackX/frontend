0317b0959a54976d3e2a7e2c551dbec46c6d797e
Artúr Friedrich <artur.friedrich.harka@gmail.com>
2023-10-25 21:05:57 +0200

    (footer): improved footer style

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,13 +1,25 @@
-import React from 'react';
-
-import "./footer.css"
+import { Link } from "react-router-dom";
+import "./footer.css";
 
 export const Footer = () => {
-    return (
-        <footer className="bg-dark text-light text-center py-3 custom-footer">
-            <div className="container">
-            <p>&copy; {new Date().getFullYear()} PackX - All rights reserved - <a href="/policy">Terms and conditions</a></p>
+  return (
+    <footer className="bg-dark text-light py-3 custom-footer">
+      <div className="container">
+        <div className="footer-content">
+          <div className="footer-left">
+            <p>Email: info@packx.com</p>
+            <p>Phone: +36 20 123 45 67</p>
+          </div>
+          <div className="footer-right">
+            <div className="address">
+              <p>Tibormajori utca 70</p>
+              <p>Győr, Hungary</p>
             </div>
-        </footer>
-    );
+            <Link className="footer-link" to="/contact">Contact Us</Link>
+          </div>
+        </div>
+        <p className="copyright">&copy; {new Date().getFullYear()} PackX - All rights reserved - <Link  className="footer-link" to="/policy">Terms and conditions</Link></p>
+      </div>
+    </footer>
+  );
 }

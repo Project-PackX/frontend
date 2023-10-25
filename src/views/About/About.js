ee46d0df99c197b119ca792e2c6f9c3f5683c877
MrHumanRebel <szekelydani5g@gmail.com>
2023-10-25 14:08:45 +0200

    feat(contact): load people

--- src/views/About/About.js
+++ src/views/About/About.js
@@ -42,27 +42,27 @@ export const About = () => {
                 <h1 className="about-title">Designed and created by</h1>
                 <div className="row justify-content-center logo-container">
                     <div className="col-md-2 logo-column">
-                        <img src={require("../../assets/images/undraw_takeout_boxes_ap54.svg").default} alt="Szakály Károly" className="img-fluid rounded-circle" />
+                        <img src={require("../../assets/people/karcsi.jpg")} alt="Szakály Károly" className="img-profile" />
                         <a href="https://github.com/szkly" className="about-data">Szakály Károly</a>
                         <p className="about-data">DevOps and Backend</p>
                     </div>
                     <div className="col-md-2 logo-column">
-                        <img src={require("../../assets/images/undraw_takeout_boxes_ap54.svg").default} alt="Friedrich Artúr" className="img-fluid rounded-circle" />
+                        <img src={require("../../assets/people/artur.jpg")} alt="Friedrich Artúr" className="img-profile" />
                         <a href="https://github.com/arturfriedrich" className="about-data">Friedrich Artúr</a>
                         <p className="about-data">Frontend and UX Design</p>
                     </div>
                     <div className="col-md-2 logo-column">
-                        <img src={require("../../assets/images/undraw_takeout_boxes_ap54.svg").default} alt="Szilágyi Dominik" className="img-fluid rounded-circle" />
+                        <img src={require("../../assets/people/domi.jpg")} alt="Szilágyi Dominik" className="img-profile" />
                         <a href="https://github.com/dominikszilagyi" className="about-data">Szilágyi Dominik</a>
                         <p className="about-data">Backend</p>
                     </div>
                     <div className="col-md-2 logo-column">
-                        <img src={require("../../assets/images/undraw_takeout_boxes_ap54.svg").default} alt="Töreky Zsombor" className="img-fluid rounded-circle" />
+                        <img src={require("../../assets/people/zsombi.jpg")} alt="Töreky Zsombor" className="img-profile" />
                         <a href="https://github.com/tzsombi01" className="about-data">Töreky Zsombor</a>
                         <p className="about-data">Backend</p>
                     </div>
                     <div className="col-md-2 logo-column">
-                        <img src={require("../../assets/images/undraw_takeout_boxes_ap54.svg").default} alt="Székely Dániel" className="img-fluid rounded-circle" />
+                        <img src={require("../../assets/people/danci.jpg")} alt="Székely Dániel" className="img-profile" />
                         <a href="https://github.com/MrHumanRebel" className="about-data">Székely Dániel</a>
                         <p className="about-data">Frontend and UX Design</p>
                     </div>

b3a5a978f8ab0885eed24ed9c8d7ed0620ff762a
MrHumanRebel <szekelydani5g@gmail.com>
2023-10-25 14:22:51 +0200

    feat(history): handle no history

--- src/views/History/History.js
+++ src/views/History/History.js
@@ -26,28 +26,36 @@ export const History = () => {
 
     return (
         <div className="history-container">
-            <h1>History</h1>
-            <div className="row">
-                {history.map((item, index) => (
-                    <div className="col-md-4" key={index}>
-                        <div className="history-card">
-                            <div className="history-item">
-                                <h5 className="card-title">Package ID: {item.ID}</h5>
-                                <p className="card-text">Created At: {item.CreatedAt}</p>
-                                <p className="card-text">Track ID: {item.TrackID}</p>
-                                <p className="card-text">Sender Locker ID: {item.SenderLockerId}</p>
-                                <p className="card-text">Destination Locker ID: {item.DestinationLockerId}</p>
-                                <p className="card-text">Price: {item.Price}</p>
-                                <p className="card-text">Delivery Date: {item.DeliveryDate}</p>
-                                <p className="card-text">Note: {item.Note}</p>
+            {history.length > 0 && <h1>History</h1>}
+            {history.length > 0 ? ( // Check if there is history data
+                <div className="row">
+                    {history.map((item, index) => (
+                        <div className="col-md-4" key={index}>
+                            <div className="history-card">
+                                <div className="history-item">
+                                    <h5 className="card-title">Package ID: {item.ID}</h5>
+                                    <p className="card-text">Created At: {item.CreatedAt}</p>
+                                    <p className="card-text">Track ID: {item.TrackID}</p>
+                                    <p className="card-text">Sender Locker ID: {item.SenderLockerId}</p>
+                                    <p className="card-text">Destination Locker ID: {item.DestinationLockerId}</p>
+                                    <p className="card-text">Price: {item.Price}</p>
+                                    <p className="card-text">Delivery Date: {item.DeliveryDate}</p>
+                                    <p className="card-text">Note: {item.Note}</p>
+                                </div>
+                                <Link to={`/track/${item.TrackID}`} className="btn login-btn">
+                                    Track
+                                </Link>
                             </div>
-                            <Link to={`/track/${item.TrackID}`} className="btn login-btn">
-                                Track
-                            </Link>
                         </div>
-                    </div>
-                ))}
-            </div>
+                    ))}
+                </div>
+            ) : ( // If there is no history data, display the image and text
+                <div className="no-history">
+                    <img src={require("../../assets/images/undraw_void_-3-ggu.svg").default} alt="login" />
+                    <h1>You have not sent a package with us yet.</h1>
+                    <Link to="/dispatch" className="history-btn ">Send now</Link>
+                </div>
+            )}
         </div>
     );
 };

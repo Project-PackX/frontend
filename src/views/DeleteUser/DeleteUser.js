79c9da4532c2306d0ce55d14f6b04313e950b84a
Artúr Friedrich <artur.friedrich.harka@gmail.com>
2023-10-25 21:44:02 +0200

    (deleteuser): improved style

--- src/views/DeleteUser/DeleteUser.js
+++ src/views/DeleteUser/DeleteUser.js
@@ -126,7 +126,7 @@ export const DeleteUser = () => {
                 </form>
             </div>
             <div className="col-md-6">
-                <img src={require("../../assets/images/undraw_throw_away_re_x60k.svg").default} alt="user-data" />
+                <img className="image" src={require("../../assets/images/undraw_throw_away_re_x60k.svg").default} alt="user-data" />
             </div>
         </div>
     );

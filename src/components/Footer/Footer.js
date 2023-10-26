b3420446d273565abb901ab29875ae2e68da1ece
MrHumanRebel <szekelydani5g@gmail.com>
2023-10-26 15:16:15 +0200

    (footer): add mini packx logo

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -7,19 +7,36 @@ export const Footer = () => {
       <div className="container">
         <div className="footer-content">
           <div className="footer-left">
-            <p>Email: info@packx.com</p>
-            <p>Phone: +36 20 123 45 67</p>
+            <div className="contact-info">
+              <p>Email: info@packx.com</p>
+              <p>Phone: +36 20 123 45 67</p>
+            </div>
+          </div>
+          <div className="footer-center">
+            <Link className="footer-link" to="/">
+            <img className="foot-logo" src={require("../../assets/logos/packx_trans.png")} alt="logo" />
+            </Link>
           </div>
           <div className="footer-right">
             <div className="address">
               <p>Tibormajori utca 70</p>
               <p>Győr, Hungary</p>
             </div>
-            <Link className="footer-link" to="/contact">Contact Us</Link>
+            <Link className="footer-link" to="/contact">
+              Contact Us
+            </Link>
           </div>
         </div>
-        <p className="copyright">&copy; {new Date().getFullYear()} PackX - All rights reserved - <Link  className="footer-link" to="/policy">Terms and conditions</Link></p>
+        <p className="copyright">
+          &copy; {new Date().getFullYear()} PackX - All rights reserved -{" "}
+          <Link className="footer-link" to="/policy">
+            Terms and conditions
+          </Link>
+        </p>
       </div>
     </footer>
   );
 }
+
+
+

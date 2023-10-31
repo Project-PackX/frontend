b213cabfa17177e57b4149dacbbd01661c19a92c
MrHumanRebel <szekelydani5g@gmail.com>
2023-10-31 13:04:47 +0100

    feat(packagestatus): add eur and usd

--- src/views/PackageStatus/PackageStatus.js
+++ src/views/PackageStatus/PackageStatus.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import {Link, useParams} from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import PackageDataService from '../../services/package.js';
 import { Loading } from '../../components/Loading/Loading.js';
 import './package-status.css';
@@ -15,6 +15,7 @@ export const PackageStatus = () => {
     const [packageData, setPackageData] = useState(null);
     const [isLoading, setIsLoading] = useState(true);
     const { id } = useParams();
+    const [exchangeRates, setExchangeRates] = useState(null); // State for exchange rates
 
     const getPackageStatus = (id) => {
         PackageDataService.get(id)
@@ -33,8 +34,23 @@ export const PackageStatus = () => {
 
         getPackageStatus(id);
 
+        // Fetch exchange rates when the component mounts
+        fetchExchangeRates();
+
         return () => clearTimeout(loadingTimeout);
     }, [id]);
+
+    // Function to fetch exchange rates
+    const fetchExchangeRates = () => {
+        fetch('https://open.er-api.com/v6/latest/HUF')
+            .then((response) => response.json())
+            .then((data) => {
+                setExchangeRates(data.rates);
+            })
+            .catch((error) => {
+                console.error("Error while fetching exchange rates", error);
+            });
+    };
 
     if (isLoading) {
         return <Loading className="loading" />;
@@ -51,9 +67,15 @@ export const PackageStatus = () => {
             </div>
         );
     }
+
+    // Calculate prices in different currencies
     const deliveryDate = new Date(packageData.Data.DeliveryDate);
     const dayOfWeek = deliveryDate.toLocaleDateString('en-US', { weekday: 'long' });
     const formattedDate = deliveryDate.toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' });
+
+    const hufPrice = packageData.Data.Price;
+    const eurPrice = (hufPrice * exchangeRates.EUR).toFixed(2);
+    const usdPrice = (hufPrice * exchangeRates.USD).toFixed(2);
 
     const stages = ['Dispatch', 'Transit', 'In Warehouse', 'In Delivery', 'Delivered'];
     const currentState = packageData.Status;
@@ -76,7 +98,7 @@ export const PackageStatus = () => {
             <h3>{`${dayOfWeek}, ${formattedDate}`}</h3>
             <div className="timeline">
                 {stages.map((stage, index) => (
-                    <div key={index} className="state-images"style={{
+                    <div key={index} className="state-images" style={{
                         border: index === stages.indexOf(currentState) ? '2px solid var(--purple)' : '2px solid var(--gray)'
                     }}>
                         {/* Render the SVG for the current stage and apply color conditionally */}
@@ -94,22 +116,30 @@ export const PackageStatus = () => {
             </div>
             <table className="package-table">
                 <tbody>
-                <tr>
-                    <td className="package-table-title">Tracking number:</td>
-                    <td className="package-table-info">{packageData.Data.TrackID}</td>
-                </tr>
-                <tr>
-                    <td className="package-table-title">Size:</td>
-                    <td className="package-table-info">{packageData.Data.Size}</td>
-                </tr>
-                <tr>
-                    <td className="package-table-title">Price:</td>
-                    <td className="package-table-info">{packageData.Data.Price}</td>
-                </tr>
-                <tr>
-                    <td className="package-table-title">Est. delivery date:</td>
-                    <td className="package-table-info">{formattedDate}</td>
-                </tr>
+                    <tr>
+                        <td className="package-table-title">Tracking number:</td>
+                        <td className="package-table-info">{packageData.Data.TrackID}</td>
+                    </tr>
+                    <tr>
+                        <td className="package-table-title">Size:</td>
+                        <td className="package-table-info">{packageData.Data.Size}</td>
+                    </tr>
+                    <tr>
+                        <td className="package-table-title">Price (HUF):</td>
+                        <td className="package-table-info">{hufPrice} HUF</td>
+                    </tr>
+                    <tr>
+                        <td className="package-table-title">Price (EUR):</td>
+                        <td className="package-table-info">{eurPrice} EUR</td>
+                    </tr>
+                    <tr>
+                        <td className="package-table-title">Price (USD):</td>
+                        <td className="package-table-info">{usdPrice} USD</td>
+                    </tr>
+                    <tr>
+                        <td className="package-table-title">Est. delivery date:</td>
+                        <td className="package-table-info">{formattedDate}</td>
+                    </tr>
                 </tbody>
             </table>
         </div>

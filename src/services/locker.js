962d47ea05a98e355e904d3a2b8f5725d680dd66
Artúr Friedrich <artur.friedrich.harka@gmail.com>
2023-11-01 23:17:57 +0100

    feat(locker): get packages in locker

--- src/services/locker.js
+++ src/services/locker.js
@@ -1,8 +1,19 @@
 import http from "../http-common";
 
 class LockerDataService {
+
+    token = localStorage.getItem('token');
+
     getAll() {
         return http.get("/lockers/all");
+    }
+
+    getPackages(id, token) {
+        return http.get(`/lockers/get-packages/${id}`, {
+            headers: {
+                Authorization: `${token}`
+            }
+        });
     }
 
 }

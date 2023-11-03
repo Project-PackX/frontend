b22b9f910c1c17e7dbc24463eb78999a138e4e38
Artúr Friedrich <artur.friedrich.harka@gmail.com>
2023-11-03 15:11:29 +0100

    feat(courier): cleared warning

--- src/views/Courier/CourierPackages/CourierPackages.js
+++ src/views/Courier/CourierPackages/CourierPackages.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import PackageService from "../../../services/package";
-import decode from "jwt-decode";
 import {Link} from "react-router-dom";
 import {useAuth} from "../../../context/auth";
 import {NoPermission} from "../../../components/Slave/NoPermission/NoPermission";

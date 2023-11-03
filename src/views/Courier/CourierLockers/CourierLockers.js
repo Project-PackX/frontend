44f35b0412aba17d94d3f5c66cfc23fe01912bbf
Artúr Friedrich <artur.friedrich.harka@gmail.com>
2023-11-03 14:41:58 +0100

    feat(courier-locker): sort lockers by name or by fullness #94

--- src/views/Courier/CourierLockers/CourierLockers.js
+++ src/views/Courier/CourierLockers/CourierLockers.js
@@ -5,10 +5,51 @@ import LockerDataService from "../../../services/locker";
 import "./courierlockers.css";
 
 export function CourierLockers() {
-    const token = localStorage.getItem("token");
 
     const [lockers, setLockers] = useState([]);
     const [fullness, setFullness] = useState([]);
+    const [sortCriteria, setSortCriteria] = useState(null);
+    const [sortOrder, setSortOrder] = useState("asc"); // "asc" for ascending, "desc" for descending
+
+    const sortLockersByName = () => {
+        setSortCriteria("name");
+        setSortOrder(sortOrder === "asc" ? "desc" : "asc");
+        sortLockers();
+    };
+
+    const sortLockersByFullness = () => {
+        setSortCriteria("fullness");
+        setSortOrder(sortOrder === "asc" ? "desc" : "asc");
+        sortLockers();
+    };
+
+    // Sorting logic
+    const sortLockers = () => {
+        const sortedLockers = [...lockers];
+
+        if (sortCriteria === "name") {
+            sortedLockers.sort((a, b) => {
+                if (sortOrder === "asc") {
+                    return a.City.localeCompare(b.City);
+                } else {
+                    return b.City.localeCompare(a.City);
+                }
+            });
+        } else if (sortCriteria === "fullness") {
+            sortedLockers.sort((a, b) => {
+                const fullnessA = fullness[a.ID - 1];
+                const fullnessB = fullness[b.ID - 1];
+
+                if (sortOrder === "asc") {
+                    return fullnessA - fullnessB;
+                } else {
+                    return fullnessB - fullnessA;
+                }
+            });
+        }
+
+        setLockers(sortedLockers);
+    };
 
     function getProgressBarColorClass(percentage) {
         if (percentage <= 50) {
@@ -43,9 +84,22 @@ export function CourierLockers() {
 
     return (
         <div className="container my-5">
-                <h1>Lockers</h1>
+            <div className="col-12">
                 <div className="row">
-                {lockers.map((locker, index) => (
+                    <h1 className="col-md-7">Lockers</h1>
+                    <button className="btn submit-btn me-3 col-md-2" onClick={sortLockersByName}>
+                        Sort by Name {sortCriteria === "name" && (sortOrder === "asc" ? "▲" : "▼")}
+                    </button>
+
+                    <button className="btn submit-btn col-md-2" onClick={sortLockersByFullness}>
+                        Sort by Fullness {sortCriteria === "fullness" && (sortOrder === "asc" ? "▲" : "▼")}
+                    </button>
+                </div>
+            </div>
+
+                <div className="row">
+
+                    {lockers.map((locker, index) => (
                     <div className="col-md-6" key={index}>
                         <div className="history-card">
                             <div className="history-item">

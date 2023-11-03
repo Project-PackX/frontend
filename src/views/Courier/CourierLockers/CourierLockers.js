741a565094c9e6d4d2cebdb342b9562fab187931
Artúr Friedrich <artur.friedrich.harka@gmail.com>
2023-11-03 10:46:06 +0100

    feat(home): new layout and progress bar

--- src/views/Courier/CourierLockers/CourierLockers.js
+++ src/views/Courier/CourierLockers/CourierLockers.js
@@ -1,71 +1,92 @@
-import React, {useEffect, useState} from "react";
-import {Link} from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import LockerDataService from "../../../services/locker";
 
-export function CourierLockers() {
+import "./courierlockers.css";
 
-    const token = localStorage.getItem('token');
+export function CourierLockers() {
+    const token = localStorage.getItem("token");
 
     const [lockers, setLockers] = useState([]);
     const [fullness, setFullness] = useState([]);
 
+    function getProgressBarColorClass(percentage) {
+        if (percentage <= 50) {
+            return "green-bar";
+        } else if (percentage <= 70) {
+            return "yellow-bar";
+        } else {
+            return "red-bar";
+        }
+    }
+
     const getAllLockers = () => {
         LockerDataService.getAll()
-        .then((response) => {
-            setLockers(response.data.lockers);
-            setFullness(response.data.percents);
-        })
-        .catch((error) => {
-            console.error("Error while loading lockers", error);
-        });
-    }
+            .then((response) => {
+                setLockers(response.data.lockers);
+                setFullness(response.data.percents);
+            })
+            .catch((error) => {
+                console.error("Error while loading lockers", error);
+            });
+    };
 
     useEffect(() => {
         getAllLockers();
     }, []);
 
+    const calculateProgressBarStyle = (percentage) => {
+        return {
+            width: percentage + "%",
+        };
+    };
+
     return (
         <div>
-        <h1>Courier Lockers</h1>
-        <table className="table">
-            <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>City</th>
-                    <th>Address</th>
-                    <th>Capacity</th>
-                    <th>Fullness</th>
-                    <th>Used</th>
-                    <th>Packages</th>
-                    <th>Map</th>
-                </tr>
-            </thead>
-            <tbody>
-                {lockers.map((locker) => (
-                    <tr key={locker.ID}>
-                        <td>{locker.ID}</td>
-                        <td>{locker.City}</td>
-                        <td>{locker.Address}</td>
-                        <td>{locker.Capacity}</td>
-                        <td>{fullness[locker.ID - 1]} %</td>
-                        <td>{locker.Used}</td>
-                        <td>
-                            <Link className="btn submit-btn" to={`/locker/packages/${locker.ID}`}>Show Packages</Link>
-                        </td>
-                        <td>
+            <h1>Courier Lockers</h1>
+            <div className="history-container">
+                <div className="row">
+                {lockers.map((locker, index) => (
+                    <div className="col-md-4" key={index}>
+                        <div className="history-card">
+                            <div className="history-item">
+                                <h5 className="card-title">Locker ID: {locker.ID}</h5>
+                                <p className="card-text">City: {locker.City}</p>
+                                <p className='card-text'>Locker Address: {locker.Address}</p>
+                                <p className="card-text">Capacity: {locker.Capacity}</p>
+                                <div className="progress">
+                                    <div
+                                        className={`progress-bar ${getProgressBarColorClass(fullness[locker.ID - 1])}`}
+                                        style={calculateProgressBarStyle(fullness[locker.ID - 1])}
+                                    >
+                                        {fullness[locker.ID - 1]}%
+                                    </div>
+                                </div>
+                            </div>
+                            <Link
+                                className="btn submit-btn me-3"
+                                to={`/locker/packages/${locker.ID}`}
+                            >
+                                Show Packages
+                            </Link>
                             <a
-                                href={"https://www.google.com/maps/place/" + locker.City + "+" + locker.Address}
+                                href={
+                                    "https://www.google.com/maps/place/" +
+                                    locker.City +
+                                    "+" +
+                                    locker.Address
+                                }
                                 target="_blank"
                                 rel="noreferrer"
                                 className="btn submit-btn"
                             >
                                 Map
                             </a>
-                        </td>
-                    </tr>
+                        </div>
+                    </div>
                 ))}
-            </tbody>
-        </table>
+                </div>
+            </div>
         </div>
-    )
+    );
 }

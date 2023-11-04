b844ec2006dd4d6517261e628db636645a944722
MrHumanRebel <szekelydani5g@gmail.com>
2023-11-04 23:25:23 +0100

    feat(loyalty): add page

--- src/views/Loyalty/Loyalty.js
+++ src/views/Loyalty/Loyalty.js
@@ -1,9 +1,92 @@
-import "./loyalty.css";
+import React from 'react';
+import './loyalty.css';
+
+import RegularSvg from '../../assets/logos/regular.svg';
+import BaronSvg from '../../assets/logos/baron.svg';
+import PrinceSvg from '../../assets/logos/prince.svg';
+import KingSvg from '../../assets/logos/king.svg';
 
 export const Loyalty = () => {
-    return (
-        <div>
-            <h1>Loyalty</h1>
-        </div>
-    );
-}
+  const currentOrderNumber = 15;
+  const loyaltyState = getLoyaltyState(currentOrderNumber);
+  const nextTier = getNextTier(currentOrderNumber);
+
+  function getLoyaltyState(currentOrderNumber) {
+    if (currentOrderNumber >= 15) {
+      return 'King';
+    } else if (currentOrderNumber >= 10) {
+      return 'Prince';
+    } else if (currentOrderNumber >= 5) {
+      return 'Baron';
+    } else {
+      return 'Regular';
+    }
+  }
+
+  function getNextTier(currentOrderNumber) {
+    if (currentOrderNumber >= 15) {
+      return 'You have reached the highest tier.';
+    } else if (currentOrderNumber >= 10) {
+      return 'King';
+    } else if (currentOrderNumber >= 5) {
+      return 'Prince';
+    } else {
+      return 'Baron';
+    }
+  }
+
+  const getSvgForLoyaltyLevel = (level) => {
+    const svgMap = {
+      'Regular': RegularSvg,
+      'Baron': BaronSvg,
+      'Prince': PrinceSvg,
+      'King': KingSvg,
+    };
+    return svgMap[level];
+  };
+
+  const getStateMinOrder = (level) => {
+    const minMap = {
+      'Baron': 5,
+      'Prince': 10,
+      'King': 15,
+    };
+    return minMap[level];
+  };
+
+  const getLoyaltyPercentage = (level) => {
+    const percentageMap = {
+      'Regular': 0,
+      'Baron': 5,
+      'Prince': 7.5,
+      'King': 10,
+    };
+    return percentageMap[level];
+  };
+
+  const loyaltyDetails = (
+    <div className="loyalty-details container">
+      <h2>Loyalty Status: {loyaltyState}</h2>
+      <div className="loyalty-logo">
+        <img
+          src={getSvgForLoyaltyLevel(loyaltyState)}
+          alt={loyaltyState}
+          className="loyalty-image"
+        />
+      </div>
+      <p>
+        {nextTier === 'You have reached the highest tier.'
+          ? nextTier
+          : `You are ${getStateMinOrder(nextTier) - currentOrderNumber} orders away from becoming a ${nextTier}.`}
+      </p>
+
+        {loyaltyState !== 'Regular' && (
+        <p>
+          Current tier benefit: {getLoyaltyPercentage(loyaltyState)}% discount on all orders
+        </p>
+      )}
+    </div>
+  );
+
+  return loyaltyDetails;
+};

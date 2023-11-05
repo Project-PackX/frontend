9e2f01904af5a5d343422d0a937a7b3ecae42e37
MrHumanRebel <szekelydani5g@gmail.com>
2023-11-05 10:33:51 +0100

    fix(loyalty): optimise code

--- src/views/Loyalty/Loyalty.js
+++ src/views/Loyalty/Loyalty.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { useAuth } from '../../context/auth';
 import './loyalty.css';
 import { NoPermission } from '../../components/Slave/NoPermission/NoPermission';
-
 import RegularSvg from '../../assets/logos/regular.svg';
 import BaronSvg from '../../assets/logos/baron.svg';
 import PrinceSvg from '../../assets/logos/prince.svg';
@@ -14,86 +13,39 @@ export const Loyalty = () => {
   const loyaltyState = getLoyaltyState(currentOrderNumber);
   const nextTier = getNextTier(currentOrderNumber);
 
-  function getLoyaltyState(currentOrderNumber) {
-    if (currentOrderNumber >= 7) {
-      return 'King';
-    } else if (currentOrderNumber >= 5) {
-      return 'Prince';
-    } else if (currentOrderNumber >= 3) {
-      return 'Baron';
-    } else {
-      return 'Regular';
-    }
+  function getLoyaltyState(n) {
+    if (n >= 7) return 'King';
+    if (n >= 5) return 'Prince';
+    if (n >= 3) return 'Baron';
+    return 'Regular';
   }
 
-  function getNextTier(currentOrderNumber) {
-    if (currentOrderNumber >= 7) {
-      return 'You have reached the highest tier.';
-    } else if (currentOrderNumber >= 5) {
-      return 'King';
-    } else if (currentOrderNumber >= 3) {
-      return 'Prince';
-    } else {
-      return 'Baron';
-    }
+  function getNextTier(n) {
+    if (n >= 7) return 'You have reached the highest tier.';
+    if (n >= 5) return 'King';
+    if (n >= 3) return 'Prince';
+    return 'Baron';
   }
 
-  const getSvgForLoyaltyLevel = (level) => {
-    const svgMap = {
-      'Regular': RegularSvg,
-      'Baron': BaronSvg,
-      'Prince': PrinceSvg,
-      'King': KingSvg,
-    };
-    return svgMap[level];
-  };
+  const getSvg = (level) => ({ Regular: RegularSvg, Baron: BaronSvg, Prince: PrinceSvg, King: KingSvg }[level]);
 
-  const getStateMinOrder = (level) => {
-    const minMap = {
-      'Baron': 3,
-      'Prince': 5,
-      'King': 7,
-    };
-    return minMap[level];
-  };
+  const getStateMinOrder = (level) => ({ Baron: 3, Prince: 5, King: 7 }[level]);
+  const getLoyaltyPercentage = (level) => ({ Regular: 0, Baron: 5, Prince: 7.5, King: 10 }[level]);
 
-  const getLoyaltyPercentage = (level) => {
-    const percentageMap = {
-      'Regular': 0,
-      'Baron': 5,
-      'Prince': 7.5,
-      'King': 10,
-    };
-    return percentageMap[level];
-  };
+  if (!isLoggedIn) return <NoPermission />;
 
-  if (!isLoggedIn) {
-    return <NoPermission />;
-  }
-
-  const loyaltyDetails = (
+  return (
     <div className="loyalty-details container">
       <h2>Loyalty Status: {loyaltyState}</h2>
       <div className="loyalty-logo">
-        <img
-          src={getSvgForLoyaltyLevel(loyaltyState)}
-          alt={loyaltyState}
-          className="loyalty-image"
-        />
+        <img src={getSvg(loyaltyState)} alt={loyaltyState} className="loyalty-image" />
       </div>
       <p>
         {nextTier === 'You have reached the highest tier.'
           ? nextTier
           : `You are ${getStateMinOrder(nextTier) - currentOrderNumber} orders away from becoming a ${nextTier}.`}
       </p>
-
-        {loyaltyState !== 'Regular' && (
-        <p>
-          Current tier benefit: {getLoyaltyPercentage(loyaltyState)}% discount on all orders
-        </p>
-      )}
+      {loyaltyState !== 'Regular' && <p>Current tier benefit: {getLoyaltyPercentage(loyaltyState)}% discount on all orders</p>}
     </div>
   );
-
-  return loyaltyDetails;
 };

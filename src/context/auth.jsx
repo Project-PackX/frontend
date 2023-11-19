65cba2f5e63f8fd21f193d48adb141fbaa44fdbb
MrHumanRebel <szekelydani5g@gmail.com>
2023-11-19 17:56:51 +0100

    fix(login): deprecated login detection

--- src/context/auth.jsx
+++ src/context/auth.jsx
@@ -3,7 +3,31 @@ import React, { createContext, useContext, useState } from 'react';
 const AuthContext = createContext();
 
 export const useAuth = () => {
-    return useContext(AuthContext);
+    if (localStorage.getItem("login_date") !== null) {
+        const current_date = new Date().toISOString();
+        const login_date = localStorage.getItem("login_date");
+        const diff = Math.abs(new Date(current_date) - new Date(login_date));
+        // If the user logged in more than 1 hour ago, the login is deprecated
+        if (diff > 3600000) {
+            // Remove all data from localStorage except the exchange rates
+            localStorage.removeItem("token");
+            localStorage.removeItem("name");
+            localStorage.removeItem("email");
+            localStorage.removeItem("address");
+            localStorage.removeItem("phone");
+            localStorage.removeItem("user_id");
+            localStorage.removeItem("login_date");
+            localStorage.removeItem("access_level");
+            localStorage.removeItem("historyCount");
+            return;
+        }
+        else {
+            return useContext(AuthContext);
+        }
+    }
+    else {
+        return useContext(AuthContext);
+    }
 };
 
 export const AuthProvider = ({ children }) => {
